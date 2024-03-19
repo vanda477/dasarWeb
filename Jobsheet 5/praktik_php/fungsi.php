1447dee87f34f454d6f5a934ce1fd9c56adb0ece
Sheva Ananda Arifiansyah <arifiansyahshevaananda@gmail.com>
2024-03-19 13:29:22 +0700

    Pertemuan 5 - praktikum 3 memanggil fungsi di dalam fungsi

--- Jobsheet 5/praktik_php/fungsi.php
+++ Jobsheet 5/praktik_php/fungsi.php
@@ -1,25 +1,40 @@
-<!-- <?php
-function perkenalan($nama, $salam="Asslamualaikum"){
-    echo $salam.", ";
-    echo "Perkenalkan, nama saya ".$nama."<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
+<?php
+// function perkenalan($nama, $salam="Asslamualaikum"){
+//     echo $salam.", ";
+//     echo "Perkenalkan, nama saya ".$nama."<br/>";
+//     echo "Senang berkenalan dengan Anda<br/>";
 
-}
-perkenalan("Hamdana","Hallo");
+// }
+// perkenalan("Hamdana","Hallo");
 
-echo"<hr>";
+// echo"<hr>";
 
-$saya ="Sheva";
-$ucapanSalam = "Selamat pagi";
+// $saya ="Sheva";
+// $ucapanSalam = "Selamat pagi";
 
-perkenalan($saya);
+// perkenalan($saya);
 
-?> -->
+?>
+
+<?php
+// function hitungUmur($thn_lahir, $thn_sekarang){
+//     $umur = $thn_sekarang - $thn_lahir;
+//     return $umur;
+// }
+// echo "Umur saya adalah". hitungUmur(2004, 2023) ."tahun"
+?>
 
 <?php
 function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
-echo "Umur saya adalah". hitungUmur(2004, 2023) ."tahun"
+function perkenalan($nama, $salam="Asslamualaikum"){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+
+    echo "Saya berusia ". hitungUmur(2004, 2023) ." tahun<br/>";
+    echo "Senang berkenalan dengan anda<br>";
+}
+perkenalan("Sheva");
 ?>
