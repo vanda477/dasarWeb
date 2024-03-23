24d4851b2b2e2a770e54932af4799391baf3ac91
Sheva Ananda Arifiansyah <arifiansyahshevaananda@gmail.com>
2024-03-23 12:36:53 +0700

    peretmuan 5 -prak 3 html dan php

---
+++ Jobsheet 5/praktik_php/html_php.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Cara 01</title>
+    </head>
+    <body>
+        <p>Tanggal Hari ini : <?php echo date("d M Y");?></p>
+    </body>
+</html>
+
+<?php
+echo '<html>';
+echo '<head><title>Cara 02</title></head>';
+echo '<body>';
+echo '<p>Tanggal Hari ini: ' . date('d M Y') . '</p>';
+echo '</body>';
+echo '</html>';
+?>
