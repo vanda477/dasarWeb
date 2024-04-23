cf9542aa0869770856b285c031b06afe74814e88
Sheva Ananda Arifiansyah <arifiansyahshevaananda@gmail.com>
2024-04-23 10:40:29 +0700

    Pertemuan 11 - prak 3 folder template

--- Jobsheet 11/config/koneksi.php
+++ Jobsheet 11/config/koneksi.php
@@ -1,7 +1,8 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
-$koneksi = mysqli_connect("localhost", "root", null, "prakwebdb");
+$koneksi = mysqli_connect("localhost", "root", "123456", "prakwebdb");
 
-if (mysqli_connect_error()) {
+if (mysqli_connect_errno()) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
+?>
